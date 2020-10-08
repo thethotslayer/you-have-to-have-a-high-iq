a2930f52bbd99199ecf1debaa900857be1bc1525
thethotslayer <72532352+thethotslayer@users.noreply.github.com>
2020-10-07 17:51:36 -0700

    Add files via upload

---
+++ Ide (3).java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        int x = 1;
+        System.out.println("to be fair you have to have a high iq to understand rick and morty");
+        System.out.println("the humor is very subtle and with out a solid grasp theoretical phyiscs most jokes will");
+        System.out.println("go over the typical veiwers head. There is also rick's nihilistic out look ");
+        System.out.println("which is woven into his characterisation- his personal philosophy draws");
+        System.out.println("heavily fromNarodnaya Volya literature,or instance. The fans understand this stuff;");
+        System.out.println("they have to have the intellecual capcity to truely apreciate the depths of these jokes,");
+        System.out.println("to realize that there not just funny but they say something deep about LIFE");
+        System.out.println("As a consequence, poeple who do not like rick and morty truely are IDIOTS - of course ");
+        System.out.println("they wouldnt apreciate, for instance the humor in ricks existential catchphrase,");
+        System.out.println("WUBBALUBBA DUB DUB, which its self is a cryptic refrence to turgenev's russian epic ");
+        System.out.println("Fathers and Sons. Im smirking right now just imagining one of those addlepated simpleton");
+        System.out.println("scratching their heads in confusion as Dan Harmon's genius unfolds itself on there ");
+        System.out.println("television screens. what fools, how I pity them. and yes by the way i DO have a rick and morty");
+        System.out.println("tatoo. you cannot see its. Its only for the ladies only. and even they have to demonstrate that");
+        System.out.println("they're are in 5 IQ points of my own (preferable lower) beforehand");
+        System.out.printf("so ", args);
+do {
+  x++;
+} while(x < 99);
+System.out.println("do you think im right?");
+//despacito 
+/*
+prints all the numbers in between
+Made by TheThotslayer
+*/
+    }
+}
